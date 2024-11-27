be22eb09c21a15ce4af4f61c85f5fcbbb4d9cb36
adityanarayanan343 <adityanarayanan343@gmail.com>
2024-11-27 09:00:29 +0530

    Styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,24 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <div className='container'>
-      <nav>
-        <ul>
+    <div className='container mx-auto p-4'>
+      <nav className='bg-gray-800 p-4 rounded-lg shadow-md'>
+        <ul className='flex space-x-4'>
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/" className='text-white hover:text-gray-300'>Home</Link>
           </li>
           <li>
-            <Link href="/role">Roles</Link>
+            <Link href="/role" className='text-white hover:text-gray-300'>Roles</Link>
           </li>
           <li>
-            <Link href="/permission">Permissions</Link>
+            <Link href="/permission" className='text-white hover:text-gray-300'>Permissions</Link>
           </li>
           <li>
-            <Link href="/user">Users</Link>
+            <Link href="/user" className='text-white hover:text-gray-300'>Users</Link>
           </li>
         </ul>
       </nav>
-      <h1>Welcome to the Home Page</h1>
+      <h1 className='text-3xl font-bold mt-6'>Welcome to the Home Page</h1>
     </div>
   );
 }
